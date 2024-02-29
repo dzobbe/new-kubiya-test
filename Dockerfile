0070fb26f03b0d61dc34a710dbf4243311cfc838
dzobbe <mazzeo.gio@gmail.com>
2024-02-29 15:52:20 +0000

    Add Python Redis client and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,9 @@
-FROM python:3.7-slim
-echo "##active_line2##"
+FROM python:3.7
 
-echo "##active_line3##"
 WORKDIR /app
-echo "##active_line4##"
 
-echo "##active_line5##"
 COPY . /app
-echo "##active_line6##"
 
-echo "##active_line7##"
 RUN pip install redis
-echo "##active_line8##"
 
-echo "##active_line9##"
-CMD ["python", "app.py"]
+CMD ["python", "./redis_client.py"]
--- redis_client.py
+++ redis_client.py
@@ -1,13 +1,7 @@
 import os
-echo ##active_line2##
 import redis
-echo ##active_line3##
 
-echo ##active_line4##
-REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
-echo ##active_line5##
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 
-echo ##active_line6##
 r = redis.Redis(host=REDIS_HOST, port=6379, db=0)
-echo ##active_line7##
-r.set('key', 'value')
+r.set("test", "value")
